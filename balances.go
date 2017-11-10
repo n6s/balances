2e89d8f6f95eb33c280911f050d6b04214080008
Roger Brown <rbrown@e-prepared.com>
2017-11-09 22:01:17 -0700

    initial commit

---
+++ balances.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("UCCU...")
+	fmt.Println("Reminder: Omit dollar signs")
+	var sharedSavings float64
+	fmt.Printf("Shared Savings available balance: ")
+	fmt.Scanln(&sharedSavings)
+	var sharedChecking float64
+	fmt.Printf("Shared Checking available balance: ")
+	fmt.Scanln(&sharedChecking)
+	var sharedOverdraft float64
+	fmt.Printf("Shared Overdraft balance: ")
+	fmt.Scanln(&sharedOverdraft)
+	fmt.Println()
+	fmt.Println("Bank Balances as of", time.Now())
+	fmt.Println("\nUCCU Shared Account:")
+	fmt.Printf("$%.0f Shared Savings (1211627-0)\n", sharedSavings)
+	fmt.Printf("$%.0f Shared Checking (1211627-9)\n", sharedChecking)
+	if sharedOverdraft > 0 {
+		fmt.Printf("$%.0f Shared Overdraft (1211627-99)", sharedOverdraft)
+	}
+	fmt.Println()
+}
